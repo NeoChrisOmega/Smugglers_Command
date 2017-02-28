1b60bc30fa02c4426fdab8378d289e41e0890667
NeoChrisOmega <nightmenace@snet.net>
2017-02-28 10:33:46 -0500

    Actually adding the silly killObject and KillShip.cs

---
+++ Capstone2017/Assets/Scipts/Tutorials/KillObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillObject : MonoBehaviour
+{/*This is attached to the object, the object should be the solid object, 
+    so that the AI can be the trigger colliders*/
+    
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {//This will destroy the object when it goes too far out in space
+        if (this.transform.position.z <= -900)
+        {
+            Destroy(this.gameObject);
+        }	
+	}
+}
---
+++ Capstone2017/Assets/Scipts/Tutorials/KillShip.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillShip : MonoBehaviour
+{
+    public Rigidbody bullet;//Makes it have physics
+    public float bulletExit;
+    public MeshRenderer MeshMe;
+    Quaternion rotationB;
+
+    // Use this for initialization
+    void Start()
+    {
+        MeshMe.material = MeshMe.materials[0];
+        bulletExit = this.transform.position.z + 1.5f;
+        InvokeRepeating("Shoot", 5, 3);
+        rotationB.eulerAngles = new Vector3(this.transform.eulerAngles.x, this.transform.eulerAngles.y + 90, this.transform.eulerAngles.z);
+    }
+
+    void Shoot()
+    {
+        StartCoroutine(Shooting());
+    }
+
+    IEnumerator Shooting()
+    {//                                                                          This mess makes it one z position farther than where it was before
+        Rigidbody newBullet = Instantiate(bullet, new Vector3(this.transform.position.x, this.transform.position.y, bulletExit), bullet.rotation) as Rigidbody;
+        newBullet.AddRelativeForce(this.transform.forward, ForceMode.VelocityChange);
+        MeshMe.material = MeshMe.materials[2];
+        float counter = 0f;
+        float length = .1f;
+        Debug.Log("Before calling Mesh");
+        while (counter <= length)
+        {//keeps the loop going until real time has met the desired length
+            counter += Time.deltaTime;
+            yield return null;
+        }
+        Debug.Log("Called Mesh");
+        MeshMe.material = MeshMe.materials[1];
+    }
+    void OnTriggerEnter(Collider col)
+    {//Checks to see if you hit, if you did destroys it
+        Destroy(this.gameObject);
+    }
+}
